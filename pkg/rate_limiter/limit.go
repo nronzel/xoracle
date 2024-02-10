feb09ea297c006f78a9d5fc4530ffd2504c10f46
nronzel <86695181+nronzel@users.noreply.github.com>
2024-02-10 16:11:38 -0500

    refactor: got rid of global vars, used struct and methods

--- pkg/rate_limiter/limit.go
+++ pkg/rate_limiter/limit.go
@@ -21,29 +21,49 @@ type visitor struct {
 	lastSeen time.Time
 }
 
-// Create a map to hold the rate limiters for each visitor and a mutex
-var visitors = make(map[string]*visitor)
-var mu sync.RWMutex
+type RateLimiter struct {
+	visitors map[string]*visitor
+	mu       sync.RWMutex
+	rate     rate.Limit
+	burst    int
+}
 
-// Run a background goroutine to remove old entries from the visitors map to
-// prevent unbounded growth.
-func init() {
-	go cleanupVisitors()
+func NewRateLimiter(rate rate.Limit, burst int) *RateLimiter {
+	rl := &RateLimiter{
+		visitors: make(map[string]*visitor),
+		rate:     rate,
+		burst:    burst,
+	}
+	go func() {
+		for {
+			time.Sleep(1 * time.Minute)
+			rl.CleanupVisitors()
+		}
+	}()
+	return rl
 }
 
 // Retrieve and return the rate limiter for the current user if it already
 // exists. Otherwise create a new rate limiter and add it to the visitors map,
 // using IP address as key.
-func getVisitor(ip string) *rate.Limiter {
-	mu.RLock()
-	defer mu.Unlock()
+func (rl *RateLimiter) getVisitor(ip string) *rate.Limiter {
+	rl.mu.RLock()
+	v, exists := rl.visitors[ip]
+	rl.mu.RUnlock()
 
-	v, exists := visitors[ip]
+	if exists {
+		return v.limiter
+	}
+
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+	// check if user was added while aquiring lock
+	v, exists = rl.visitors[ip]
 	if !exists {
-        // NewLimiter(rate, burst)
-		limiter := rate.NewLimiter(1, 3)
+		// NewLimiter(rate, burst)
+		limiter := rate.NewLimiter(rl.rate, rl.burst)
 		// Include current time when creating a new visitor.
-		visitors[ip] = &visitor{limiter, time.Now()}
+		rl.visitors[ip] = &visitor{limiter, time.Now()}
 		return limiter
 	}
 	// Update the last seen time for visitor.
@@ -53,21 +73,18 @@ func getVisitor(ip string) *rate.Limiter {
 
 // Every minute check the map for visitors that haven't been seen for more
 // than 3 minutes and delete the entries.
-func cleanupVisitors() {
-	for {
-		time.Sleep(time.Minute)
-
-		mu.Lock()
-		for ip, v := range visitors {
-			if time.Since(v.lastSeen) > 3*time.Minute {
-				delete(visitors, ip)
-			}
+func (rl *RateLimiter) CleanupVisitors() {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+	for ip, v := range rl.visitors {
+		if time.Since(v.lastSeen) > 3*time.Minute {
+			delete(rl.visitors, ip)
 		}
-		mu.Unlock()
 	}
 }
 
-func Limit(next http.Handler) http.Handler {
+// Wraps HTTP handler with rate limiting.
+func (rl *RateLimiter) Limit(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Get the IP for the current user
 		ip, _, err := net.SplitHostPort(r.RemoteAddr)
@@ -78,7 +95,7 @@ func Limit(next http.Handler) http.Handler {
 		}
 
 		// Call getVisitor func to retrive the rate limiter for the current user
-		limiter := getVisitor(ip)
+		limiter := rl.getVisitor(ip)
 		if !limiter.Allow() {
 			http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
 			return
